c1cc47ef1c31a572a697e1bd6ea7337ce9908033
Naveen Prasath A <naveenprasath14012004@gmail.com>
2026-08-25 02:04:45 +0530

    Add files via upload
    MULTI-PAGE-APPLICATION --BUCKS SAUCE-- HTML,CSS,JS

---
+++ bucks.js
@@ -0,0 +1,22 @@
+
+memu
+function open1(){
+	let sider = document.getElementById("sider")
+	sider.style.right="0"
+	sider.style.display="flex"
+}
+function close1(){
+	let sider = document.getElementById("sider")
+	sider.style.right="-30vw"
+}
+
+cart
+function od1(){
+	let cartbox = document.getElementById("cartbox")
+	cartbox.style.right="0"
+	cartbox.style.display="flex"
+}
+function cd1(){
+	let sider = document.getElementById("cartbox")
+	cartbox.style.right="-40vw"
+}
